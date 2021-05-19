cd769fe4cb3983f57cbb28cd5f24e7a21cd7699c
Mike Lindner <mlindner9@yahoo.com>
2021-05-19 18:28:09 -0400

    Support for document.count_objs and document.get_obj commands.

--- evapi/EvDocument.cs
+++ evapi/EvDocument.cs
@@ -18,7 +18,7 @@ namespace evapi
 
         public string GetName()
         {
-            Dict js = new Dict()
+            Dict cmd = new Dict()
             {
                 {"type", "cmd"},
                 {"cmd", "document.name"},
@@ -30,13 +30,13 @@ namespace evapi
                 }
             };
 
-            Dict output = _conn.IssueCommand(js);
+            Dict output = _conn.IssueCommand(cmd);
             return (string) output["name"];
         }
 
         public string GetPath()
         {
-            Dict js = new Dict()
+            Dict cmd = new Dict()
             {
                 {"type", "cmd"},
                 {"cmd", "document.path"},
@@ -48,8 +48,46 @@ namespace evapi
                 }
             };
 
-            Dict output = _conn.IssueCommand(js);
+            Dict output = _conn.IssueCommand(cmd);
             return (string)output["path"];
+        }
+
+        public int CountObjects()
+        {
+            Dict cmd = new Dict()
+            {
+                {"type", "cmd"},
+                {"cmd", "document.count_objs"},
+                {
+                    "args", new Dict()
+                    {
+                        {"doc_id", Id}
+                    }
+                }
+            };
+
+            Dict output = _conn.IssueCommand(cmd);
+            return (int)output["count"];
+        }
+
+        public EvObject GetObject(int idx)
+        {
+            Dict cmd = new Dict()
+            {
+                {"type", "cmd"},
+                {"cmd", "document.get_obj"},
+                {
+                    "args", new Dict()
+                    {
+                        {"doc_id", Id},
+                        {"obj_idx", idx}
+                    }
+                }
+            };
+
+            Dict output = _conn.IssueCommand(cmd);
+            string id = (string)Convert.ToDictionary(output["obj"])["id"];
+            return new EvObject(id, _conn);
         }
     }
 }
