d2a66c846ab717806309d6316805ed6c0f50ba73
Mike Lindner <mlindner9@yahoo.com>
2021-06-18 13:11:23 -0400

    Support for object property commands.

--- evapi/EvObject.cs
+++ evapi/EvObject.cs
@@ -1,16 +1,92 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace evapi
 {
+    using Dict = Dictionary<string, object>;
+
     public class EvObject
     {
-        private Connection conn_;
+        private Connection _conn;
 
         public string Id { get; }
         
         internal EvObject(string id, Connection conn)
         {
-            conn_ = conn;
+            _conn = conn;
             Id = id;
+        }
+
+        public string Property(string name)
+        {
+            Dict cmd = new Dict()
+            {
+                {"type", "cmd"},
+                {"cmd", "obj.property"},
+                {
+                    "args", new Dict()
+                    {
+                        {"obj_id", Id},
+                        {"property", name}
+                    }
+                }
+            };
+
+            Dict output = _conn.IssueCommand(cmd);
+            return (string)output["value"];
+        }
+
+        public void SetProperty(string name, string value)
+        {
+            Dict cmd = new Dict()
+            {
+                {"type", "cmd"},
+                {"cmd", "obj.set_property"},
+                {
+                    "args", new Dict()
+                    {
+                        {"obj_id", Id},
+                        {"property", name},
+                        {"value", value}
+                    }
+                }
+            };
+
+            _conn.IssueCommand(cmd);
+        }
+
+        public void RemoveProperty(string name)
+        {
+            Dict cmd = new Dict()
+            {
+                {"type", "cmd"},
+                {"cmd", "obj.remove_property"},
+                {
+                    "args", new Dict()
+                    {
+                        {"obj_id", Id},
+                        {"property", name}
+                    }
+                }
+            };
+
+            _conn.IssueCommand(cmd);
+        }
+
+        public void RemoveAllProperties()
+        {
+            Dict cmd = new Dict()
+            {
+                {"type", "cmd"},
+                {"cmd", "obj.remove_all_properties"},
+                {
+                    "args", new Dict()
+                    {
+                        {"obj_id", Id}
+                    }
+                }
+            };
+
+            _conn.IssueCommand(cmd);
         }
     }
 }
